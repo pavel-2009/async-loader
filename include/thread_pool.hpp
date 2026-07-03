87fe4f26e3e76caa9ec7342d67ef6d39f0fcf653
pavel-2009 <ms.radionova@bk.ru>
2026-07-03 16:13:53 +0500

    basic thread pool

---
+++ include/thread_pool.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <condition_variable>
+#include <functional>
+#include <jthread>
+#include <mutex>
+#include <queue>
+#include <vector>
+
+
+class ThreadPool {
+
+    private:
+        std::vector<std::jthread> workers_;
+
+        std::queue<std::function<void()>> tasks_;
+
+        std::mutex mutex;
+
+        std::condition_variable condition_;
+
+        bool stop_ = false;
+
+    private:
+
+        void worker();
+
+    public:
+
+        explicit ThreadPool(std::size_t threadCount);
+
+        ~ThreadPool();
+
+        void submit(std::function<void()> task);
+
+}
